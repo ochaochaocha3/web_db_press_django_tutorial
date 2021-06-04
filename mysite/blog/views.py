a4c0e90c46293b8fbdde432185ac6c362aac024e
Kosuke Yamashita <ochaochaocha3@gmail.com>
2021-06-04 20:27:46 +0900

    p. 58途中

---
+++ mysite/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+from blog.models import Post
+
+def post_list(request):
+  return TemplateResponse(
+    request,
+    "post_list.html",
+    {"posts": Post.objects.all()},
+  )
+
+def post_detail(request, post_id):
+  post = Post.objects.get(id=post_id)
+  return TemplateResponse(
+    request,
+    "post_detail.html",
+    {"post": post},
+  )
